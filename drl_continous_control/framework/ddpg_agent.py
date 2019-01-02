d02d1c47682383ecb040667cd6d1c53c13bf25dc
Penta <penta.antonio@accenture.com>
2019-01-03 00:27:26 +0100

    type:refactoring, chose the version 2 of the env

--- drl_continous_control/framework/ddpg_agent.py
+++ drl_continous_control/framework/ddpg_agent.py
@@ -3,47 +3,44 @@ import random
 import copy
 from collections import namedtuple, deque
 
-from framework.model import Actor, Critic
-
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+from framework.model import  Actor,Critic
+
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 256  # minibatch size
-GAMMA = 0.999  # discount factor
-TAU = 1e-3  # for soft update of target parameters
-LR_ACTOR = 1e-3  # learning rate of the actor
-LR_CRITIC = 1e-3  # learning rate of the critic
-WEIGHT_DECAY = 0  # L2 weight decay
-UPDATE_EVERY = 20  # do the learning every timestamps
-N_LEARN_UPDATES =  10 # how many time do the learning
-COUNT_SAMPLE = 5 # how many time I should sample until the rewards average is different from 0
+BATCH_SIZE = 128         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 1e-4         # learning rate of the actor
+LR_CRITIC = 1e-4        # learning rate of the critic
+WEIGHT_DECAY = 0.0      # L2 weight decay
+
+N_LEARN_UPDATES = 10     # number of learning updates
+N_TIME_STEPS = 20       # every n time step do update
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
-    """Interacts with and learns from the environment."""
-    
-    def __init__(self, state_size, action_size, random_seed,logs=None):
+
+    def __init__(self, state_size, action_size, random_seed):
         """Initialize an Agent object.
-        
+
         Params
         ======
             state_size (int): dimension of each state
             action_size (int): dimension of each action
             random_seed (int): random seed
-            logs (list): list of values to monitor the agent performances 0 actions, 1 actions+noise, 2 rewards
         """
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
 
-        # Actor Network (w/ Target Network)
         self.actor_local = Actor(state_size, action_size, random_seed).to(device)
         self.actor_target = Actor(state_size, action_size, random_seed).to(device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
-        # Critic Network (w/ Target Network)
         self.critic_local = Critic(state_size, action_size, random_seed).to(device)
         self.critic_target = Critic(state_size, action_size, random_seed).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
@@ -51,28 +48,22 @@ class Agent():
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
-        # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
-        self.t_step = 0
-
-        self.logs = logs
-
-
-    
-    def step(self, state, action, reward, next_state, done):
+    def step(self, time_step, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
         self.memory.add(state, action, reward, next_state, done)
 
-        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        # only learn every n_time_steps
+        if time_step % N_TIME_STEPS != 0:
+            return
 
-        if self.t_step == 0:
-            # Learn, if enough samples are available in memory
-            if len(self.memory) > BATCH_SIZE:
-                for _ in range(N_LEARN_UPDATES):
-                        experiences = self.memory.sample()
-                        self.learn(experiences, GAMMA)
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE:
+            for i in range(N_LEARN_UPDATES):
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -81,13 +72,8 @@ class Agent():
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
         self.actor_local.train()
-        if self.logs is not None:
-            self.logs[0].append(np.clip(action, -1, 1))
-        #print('action before noise', action)
         if add_noise:
             action += self.noise.sample()
-            self.logs[1].append(np.clip(action, -1, 1))
-            #print('action after noise', np.clip(action, -1, 1))
         return np.clip(action, -1, 1)
 
     def reset(self):
@@ -99,15 +85,12 @@ class Agent():
         where:
             actor_target(state) -> action
             critic_target(state, action) -> Q-value
-
         Params
         ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
-        if self.logs is not None:
-            self.logs[2].append(rewards.numpy().mean(axis=0))
 
         # ---------------------------- update critic ---------------------------- #
         # Get predicted next-state actions and Q values from target models
@@ -137,18 +120,14 @@ class Agent():
         self.soft_update(self.critic_local, self.critic_target, TAU)
         self.soft_update(self.actor_local, self.actor_target, TAU)
 
-        # ---------------------------- update noise ---------------------------- #
-        self.noise.reset()
-
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
         θ_target = τ*θ_local + (1 - τ)*θ_target
-
         Params
         ======
             local_model: PyTorch model (weights will be copied from)
             target_model: PyTorch model (weights will be copied to)
-            tau (float): interpolation parameter 
+            tau (float): interpolation parameter
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
@@ -190,12 +169,12 @@ class ReplayBuffer:
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
-    
+
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
-    
+
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
--- drl_continous_control/main_script_ddpg_train.py
+++ drl_continous_control/main_script_ddpg_train.py
@@ -21,47 +21,33 @@ import pandas as pd
 
 if __name__=='__main__':
 
-    #seed (int) : seed used to inizialize the network
-    #version (int): version number to save model check point and files
-    #report_value (int): report value used to average the last values
-    # max_score (float): that target score that we would like to reach, the benchmark is 15 in 1700 episode
-    # n_episodes (int): maximum number of training episodes
-    # noise_selection (boolean): if we would like to add noise to the actions (noise if True)
-    #Linux (boolean): boolan value used to run on AWS (aws if Linux = True)
-    # logs 0, action, 1 action+noise, 2 rewards
 
 
-    seed = 12
-    version = 5
+    version = 1 #version (int): version number to save model check point and files
     report_value = 10
-    n_episodes = 200
-    max_score = 30.0
+    n_episodes = 300 # n_episodes (int): maximum number of training episodes
+    max_score = 30.0  # max_score (float): that target score that we would like to reach, the benchmark is 15 in 1700 episode
+    score_window_size = 100 #score_window_size(int): buffer size for the score
 
-    noise_selection = True
-    Linux = True
+    max_t = 1000  # max_t (int) max length of the time stemps
+    time_stamp_report = 20  #time_stamp_report(int): value used to report each time stamps
 
-    logs=[[],[],[]]
-
+    noise_selection = True # noise_selection (boolean): if we would like to add noise to the actions (noise if True)
+    Linux =  False  #Linux (boolean): boolan value used to run on AWS (aws if Linux = True)
 
     file_scores = os.path.join('data','scores_'+str(version)+'.txt')
-    file_actions = os.path.join('data', 'actions_' + str(version) + '.txt')
-    file_actions_plus_noise = os.path.join('data', 'actions_noise_' + str(version) + '.txt')
-    file_rewards = os.path.join('data', 'rewards_' + str(version) + '.txt')
 
     if Linux:
         env = UnityEnvironment(file_name=os.path.join(os.getcwd(),'env', 'Reacher_Linux_NoVis','Reacher.x86_64'))
     else:
         env = UnityEnvironment(file_name=os.path.join('env','Reacher.app'))
+
     # get the default brain
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
+
     # reset the environment
     env_info = env.reset(train_mode=True)[brain_name]
-
-
-
-    ####
-
 
     # number of agents
     num_agents = len(env_info.agents)
@@ -76,71 +62,68 @@ if __name__=='__main__':
     state_size = states.shape[1]
     print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
     print('The state for the first agent looks like:', states[0])
-    ##
 
+    scores_window = deque(maxlen=score_window_size)
+    scores = np.zeros(num_agents)
+    scores_episode = []
 
+    agents = []
 
-    #instanziate the agent
-    agent = Agent(state_size=state_size, action_size=action_size, random_seed=seed,logs=logs)
-
-    # DDPG training
-
-    all_scores = []  # list containing scores from each episode
-    scores_window = deque(maxlen=report_value)  # last 100 scores
+    for i in range(num_agents):
+        agents.append(Agent(state_size, action_size, random_seed=0))
 
     for i_episode in range(1, n_episodes + 1):
-        # reset the env
         env_info = env.reset(train_mode=True)[brain_name]
-        state = env_info.vector_observations[0]
-        scores = 0
-        while True:
-            action = agent.act(state,add_noise=noise_selection)
-            env_info = env.step(action)[brain_name]  # send the action to the environment
-            next_state = env_info.vector_observations[0]  # get the next state
-            reward = env_info.rewards[0]  # get the reward
-            done = env_info.local_done[0]
-            agent.step(state, action, reward, next_state, done)
-            state = next_state
-            scores += reward
-            if done:
-                # reset the noise
-                agent.reset()
+        states = env_info.vector_observations
+
+        for agent in agents:
+            agent.reset()
+
+        scores = np.zeros(num_agents)
+
+        for t in range(max_t):
+            actions = np.array([agents[i].act(states[i],add_noise=noise_selection) for i in range(num_agents)])
+            env_info = env.step(actions)[brain_name]  # send the action to the environment
+            next_states = env_info.vector_observations  # get the next state
+            rewards = env_info.rewards  # get the reward
+            dones = env_info.local_done
+            for i in range(num_agents):
+                agents[i].step(t, states[i], actions[i], rewards[i], next_states[i], dones[i])
+
+            states = next_states
+            scores += rewards
+            if t % time_stamp_report:
+                print('\rTimestep {}\tScore: {:.2f}\tmin: {:.2f}\tmax: {:.2f}'
+                      .format(t, np.mean(scores), np.min(scores), np.max(scores)), end="")
+            if np.any(dones):
                 break
+        score = np.mean(scores)
+        scores_window.append(score)  # save most recent score
+        scores_episode.append(score)
 
-        scores_window.append(scores)
-        all_scores.append(scores)
-
-
-        if i_episode % report_value == 0:
-            print('\n')
-            print('-' * 100)
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-            print('\n')
-            print('-' * 100)
-
-        if np.mean(scores_window) >= max_score:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - report_value,
+        print('\rEpisode {}\tScore: {:.2f}\tAverage Score: {:.2f}'.format(i_episode, score, np.mean(scores_window)),
+              end="\n")
+        if np.mean(scores_window) >= 30.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - score_window_size,
                                                                                          np.mean(scores_window)))
-            torch.save(agent.actor_local.state_dict(), os.path.join('model','checkpoint_actor_'+str(version)+'.pth'))
+            torch.save(agent.actor_local.state_dict(),
+                       os.path.join('model', 'checkpoint_actor_' + str(version) + '.pth'))
             torch.save(agent.critic_local.state_dict(),
                        os.path.join('model', 'checkpoint_critic_' + str(version) + '.pth'))
+
             break
 
     if np.mean(scores_window) < max_score:
-        print('\nEnvironment not fully solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - report_value,
-                                                                                     np.mean(scores_window)))
-        torch.save(agent.actor_local.state_dict(), os.path.join('model', 'checkpoint_actor_' + str(version) + '.pth'))
-        torch.save(agent.actor_local.state_dict(), os.path.join('model', 'checkpoint_critic_' + str(version) + '.pth'))
+        print('\nEnvironment not fully solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
+                i_episode - report_value,
+                np.mean(scores_window)))
+        torch.save(agent.actor_local.state_dict(),
+                       os.path.join('model', 'checkpoint_actor_' + str(version) + '.pth'))
+        torch.save(agent.actor_local.state_dict(),
+                       os.path.join('model', 'checkpoint_critic_' + str(version) + '.pth'))
 
     with open(file_scores, 'w') as fscores:
-        fscores.write('\n'.join([str(item) for item in all_scores]))
-
-    if logs is not None:
-
-        np.savetxt(file_actions, np.vstack(logs[0]), delimiter=',',fmt='%.4f')
-        np.savetxt(file_actions_plus_noise, np.vstack(logs[1]), delimiter=',', fmt='%.4f')
-        np.savetxt(file_rewards, np.vstack(logs[2]), delimiter=',', fmt='%.4f')
-
+            fscores.write('\n'.join([str(item) for item in scores_episode]))
 
 
     env.close()
